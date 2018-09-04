620ec734dfb4abf59d312ef469132b9f256d7881
sadler2007@gmail.com <sadler2007@gmail.com>
2018-09-04 23:23:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+var destructivelyAppendKitten = function (kittens){
+  destructivelyAppendKitten = ['Milo', 'Otis', 'Garfield', 'Ralph']
+  kittens.push()
+  return destructivelyAppendKitten
+}
